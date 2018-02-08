a3c171b6fc3f6fec5bf9072fcbe7d2da4d676767
j-ornelas <johnnyoh1988@gmail.com>
2018-02-08 14:49:40 -0800

    makes page redirect to entered url(if it's in archive)

--- web/request-handler.js
+++ web/request-handler.js
@@ -30,25 +30,44 @@ var actions = {
       }
     });
   },
-  'POST': function(req, res) {
-    // console.log('POST request = ', req);
-    // console.log('POST response = ', res);      
+  'POST': function(req, res) { 
       
     var data = '';
     req.on('data', function(chunk) {
-      data += chunk;
+      data += chunk + '\n';
       data = data.slice(4);
     });
     req.on('end', function() {
-      fs.appendFile('/Users/student/code/hrsf90-web-historian/archives/sites.txt', data, (err) => {
+      fs.appendFile(__dirname + '/archives/sites.txt', data, (err) => {
         if (err) {
           throw err;
         }
+        var currentUrl = __dirname + '/archives/sites' + '/' + data.slice(0, -1);
         console.log('the data may have been appended');
+        console.log('DATA', JSON.stringify(data.slice(0, -1)));
+        console.log(__dirname + '/archives/sites/' + data);
+        fs.exists(currentUrl, (exists) => {
+          if (exists) {
+          // if not, check sites.txt => check later page
+          // if not in archive or sites.txt, add to sites.txt
+          // set up worker to periodically download from sites.txt
+            fs.readFile(currentUrl, 'utf-8', (err, data) => {
+              console.log(req.url);
+              if (err) {
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                res.end(data);
+              } else {
+                console.log('success');
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(data);
+              }
+            });
+          }
+        });
       });
     });
-    res.writeHead(302, {'Content-Type': 'text/html'});
-    res.end();
+    // res.writeHead(302, {'Content-Type': 'text/html'});
+    // res.end('test');
   },
   'OPTIONS': function(req, res) {}
 };
@@ -60,3 +79,15 @@ exports.handleRequest = function (req, res) {
     actions[req.method](req, res);
   }
 };
+
+
+// fs.readdir(__dirname + '/archives/sites', (err, files) => {
+//   files.forEach(file => {
+//     console.log('data = ', JSON.stringify(data));
+//     console.log('file = ', JSON.stringify(file));
+//     if (JSON.stringify(file + '\n') === JSON.stringify(data)) {
+      
+//     }
+//   });
+//   console.log('FALSE FALSE');
+// });
